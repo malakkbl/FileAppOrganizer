d81a331a304321e99a582c0c599c9fa6eb5c87ac
unknown <malak012426@gmail.com>
2023-12-26 20:48:10 +0100

    empty folders + by theme not precised

---
+++ main_code.py
@@ -0,0 +1,362 @@
+import sys
+import os
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QPushButton,
+    QLabel,
+    QRadioButton,
+    QLineEdit,
+    QFileDialog,
+    QPlainTextEdit,
+)
+import subprocess
+
+# File to text :
+import os
+import shutil
+from docx import Document
+import PyPDF2
+import docx2txt
+
+# Video and audio to text :
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
+import speech_recognition as sr
+
+# Image to text :
+import pytesseract
+from PIL import Image
+
+# score + move functions :
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from my_dict import theme_keywords
+
+
+class FileOrganizerApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.current_theme = ""  # To store the selected theme
+
+        # Create layout managers
+        main_layout = QVBoxLayout()
+        top_layout = QHBoxLayout()
+
+        # Create widgets
+        self.info_label = QLabel("Welcome to the Advanced File Organizer!")
+        self.extension_radio = QRadioButton("Organize by Extension")
+        self.theme_radio = QRadioButton("Organize by Theme")
+        self.folder_label = QLabel("Selected Folder:")
+        self.folder_path = QLineEdit()
+        self.browse_button = QPushButton("Browse")
+        self.organize_button = QPushButton("Organize Files")
+
+        # New widgets for theme input
+        self.theme_input = QPlainTextEdit()
+        self.theme_input.setHidden(True)
+        self.add_theme_button = QPushButton("Add Theme")
+        self.add_theme_button.setHidden(True)
+
+        # Configure widgets
+        self.folder_path.setReadOnly(True)
+        self.theme_input.setPlaceholderText("Enter themes, separated by commas")
+
+        # Add widgets to layouts
+        top_layout.addWidget(self.theme_radio)
+        top_layout.addWidget(self.extension_radio)
+        top_layout.addWidget(self.theme_input)
+        top_layout.addWidget(self.add_theme_button)
+        top_layout.addWidget(self.browse_button)
+
+        main_layout.addWidget(self.info_label)
+        main_layout.addWidget(self.folder_label)
+        main_layout.addWidget(self.folder_path)
+        main_layout.addLayout(top_layout)
+        main_layout.addWidget(self.organize_button)
+
+        # Set main layout
+        self.setLayout(main_layout)
+
+        # Connect signals to slots
+        self.browse_button.clicked.connect(self.select_folder)
+        self.organize_button.clicked.connect(self.organize_files)
+        self.theme_radio.toggled.connect(self.show_theme_input)
+        self.add_theme_button.clicked.connect(self.add_theme)
+
+        # Set window properties
+        self.setWindowTitle("Advanced File Organizer")
+        self.setGeometry(100, 100, 400, 400)
+
+    def show_theme_input(self, checked):
+        # Show or hide theme input based on radio button state
+        self.theme_input.setHidden(not checked)
+        self.add_theme_button.setHidden(not checked)
+
+    def add_theme(self):
+        self.current_theme = self.theme_input.toPlainText().strip()
+        self.theme_input.clear()
+
+    def select_folder(self):
+        folder_path = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if folder_path:
+            self.folder_path.setText(folder_path)
+
+    def remove_empty_folders(self, folder_path):
+            for root, dirs, files in os.walk(folder_path, topdown=False):
+                for dir_name in dirs:
+                    dir_path = os.path.join(root, dir_name)
+                    if not os.listdir(dir_path):
+                        os.rmdir(dir_path)
+                    
+    def organize_files(self):
+        folder_path = self.folder_path.text()
+
+        if not folder_path:
+            return
+
+        # Check Git repo initialization
+        if not os.path.exists(os.path.join(folder_path, ".git")):
+            subprocess.run(["git", "init"], cwd=folder_path)
+
+        if self.extension_radio.isChecked():
+            self.organize_by_extension(folder_path)
+
+        if self.theme_radio.isChecked():
+            user_entered_themes = [theme.strip() for theme in self.current_theme.split(",")]
+            self.organize_by_theme(folder_path, user_entered_themes)
+
+        # Commit changes
+        subprocess.run(["git", "add", "."], cwd=folder_path)
+        subprocess.run(["git", "commit", "-m", "Organized files"], cwd=folder_path)
+
+        self.update_version_list(folder_path)
+        self.info_label.setText(f"Files in {folder_path} organized!")
+
+        # Remove empty folders
+        self.remove_empty_folders(folder_path)
+        
+    def list_files(self, dir_path):
+        r = []
+        for name in os.listdir(dir_path):
+            path = os.path.join(dir_path, name)
+            if os.path.isfile(path):
+                r.append(path)
+            else:
+                r.extend(self.list_files(path))
+        return r
+
+    def organize_by_extension(self, folder_path):
+        all_files = self.list_files(folder_path)
+
+        for file_path in all_files:
+            if ".git" in file_path:
+                continue
+
+            extension_start = file_path.rfind(".")
+            filename_start = file_path.rfind(os.sep) + 1
+
+            if extension_start != -1:
+                file_extension = file_path[extension_start + 1 :].lower()
+            else:
+                file_extension = "no_extension"
+
+            filename = file_path[filename_start:]
+            dest_folder = os.path.join(folder_path, file_extension)
+
+            if not os.path.exists(dest_folder):
+                os.mkdir(dest_folder)
+
+            dest_file_path = os.path.join(dest_folder, filename)
+            base_filename = filename[: extension_start - filename_start]
+            counter = 1
+            while os.path.exists(dest_file_path):
+                dest_file_path = os.path.join(
+                    dest_folder, f"{base_filename}_{counter}.{file_extension}"
+                )
+                counter += 1
+
+            os.rename(file_path, dest_file_path)
+
+    def organize_by_theme(self, folder_path, themes: list):
+        print("THEMES:",themes)
+        # 1. Transformer chaque fichier en texte
+        documents_as_text = {}
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                filepath = os.path.join(root, file)
+                documents_as_text[filepath] = self.file_to_text(
+                    filepath
+                )  # file_to_text : Une fonction pour convertir un fichier en texte
+
+        # 3. Scoring des documents par rapport à chaque thème
+        document_scores = {}
+        for filepath, file_text in documents_as_text.items():
+            scores = {}
+            for theme, keywords in theme_keywords.items():
+                scores[theme] = self.calculate_score(file_text, keywords)
+            document_scores[filepath] = scores
+            
+        print("DOCUMENT_SCORES:",document_scores)
+        # 4. Organiser les fichiers basés sur le score le plus élevé et un seuil d'attribution
+        seuil = 0.1
+        for filepath, scores in document_scores.items():
+            print("FILEPATH:",filepath)
+            max_score_theme = max(scores, key=scores.get)
+            if scores[max_score_theme] < seuil:
+                # Déplacez le fichier dans le dossier "autre"
+                self.move_file_to_folder(filepath, "autre")
+            else:
+                # Déplacez le fichier dans le dossier du thème correspondant
+                self.move_file_to_folder(filepath, max_score_theme)
+
+    # FONCTIONS UTILISEES :
+
+    def file_to_text(self, filepath) -> str:
+        # Initialize doc as an empty string
+        doc = ""
+
+        # Obtenir l'extension du fichier :
+        _, file_extension = os.path.splitext(filepath)
+
+        # TEXT FILES :
+        if file_extension == ".txt":
+            with open(filepath, "r", encoding="utf-8") as file:
+                doc = file.read()
+
+        elif file_extension == ".docx":
+            doc = docx2txt.process(filepath)
+
+        elif file_extension == ".doc":
+            doc = docx2txt.process(filepath)
+
+        elif file_extension == ".pdf":
+            with open(filepath, "rb") as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+                text = ""
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+                doc = text
+
+        # IMAGE FILES (JPEG) using Tesseract OCR:
+        elif file_extension == ".jpg" or file_extension == ".jpeg":
+            try:
+                # Load the image
+                img = Image.open(filepath)
+
+                # Use Tesseract to perform OCR on the image
+                doc = pytesseract.image_to_string(img)
+
+            except FileNotFoundError as e:
+                return f"File not found error: {e}"
+            except Exception as ex:
+                return f"Error occurred: {ex}"
+
+        # VIDEO FILES:
+        elif file_extension == ".mp4":
+            # Convert MP4 to WAV audio file
+            audio_path = "output_audio.wav"
+            video_clip = VideoFileClip(filepath)
+            video_clip.audio.write_audiofile(audio_path)
+            doc = self.audio_to_text(audio_path)
+
+        # AUDIO FILES:
+        elif file_extension == ".mp3":
+            doc = self.audio_to_text(filepath)
+
+        # Check if doc is not None before calling lower()
+        if doc:
+            doc = doc.lower()
+
+        return doc
+
+
+    def audio_to_text(self, audio_path):
+        try:
+            # Convert MP3 to WAV using pydub
+            wav_audio_path = "output_audio.wav"
+            audio = AudioSegment.from_mp3(audio_path)
+            audio.export(wav_audio_path, format="wav")
+
+            # Recognize text from the WAV file
+            recognizer = sr.Recognizer()
+            with sr.AudioFile(wav_audio_path) as source:
+                audio_data = recognizer.record(source)
+
+            # Perform speech-to-text
+            text = recognizer.recognize_google(audio_data)
+
+            return text
+
+        except FileNotFoundError as e:
+            return f"File not found error: {e}"
+
+        except Exception as ex:
+            return f"Error occurred: {ex}"
+
+        finally:
+            # Remove the temporary WAV file
+            if os.path.exists(wav_audio_path):
+                os.remove(wav_audio_path)
+
+    def calculate_score(self, document: str, keywords: list) -> float:
+        # Combine document text and keywords for TF-IDF calculation
+        text_and_keywords = [document] + keywords
+
+        # Create a TF-IDF vectorizer
+        vectorizer = TfidfVectorizer()
+
+        # Fit and transform the text and keywords
+        tfidf_matrix = vectorizer.fit_transform(text_and_keywords)
+
+        # Calculate cosine similarity between the document and keywords
+        similarity_matrix = cosine_similarity(tfidf_matrix)
+
+        # The similarity score is the value at (0, 1) since 0 is the document and 1 is the keywords
+        similarity_score = similarity_matrix[0, 1]
+
+        return similarity_score
+
+    def move_file_to_folder(self, filepath: str, folder_name: str):
+        try:
+            dest_folder = os.path.join(os.path.dirname(filepath), folder_name)
+            os.makedirs(dest_folder, exist_ok=True)
+
+            filename = os.path.basename(filepath)
+            dest_path = os.path.join(dest_folder, filename)
+
+            base_filename, file_extension = os.path.splitext(filename)
+            counter = 1
+
+            while os.path.exists(dest_path):
+                dest_path = os.path.join(
+                    dest_folder, f"{base_filename}_{counter}{file_extension}"
+                )
+                counter += 1
+
+            shutil.move(filepath, dest_path)
+        except Exception as e:
+            print(f"Error moving file to {folder_name}: {e}")
+
+    def update_version_list(self, folder_path):
+        self.version_list.clear()
+        commit_logs = subprocess.run(
+            ["git", "log", "--oneline"], cwd=folder_path, capture_output=True, text=True
+        )
+        commits = commit_logs.stdout.strip().split("\n")
+
+        for index, commit in enumerate(commits, 1):
+            version_number = len(commits) - index + 1
+            version_name = f"Version {version_number}: {commit.split(' ', 1)[1]}"
+            item = QListWidgetItem(version_name)
+            self.version_list.addItem(item)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = FileOrganizerApp()
+    window.show()
+    sys.exit(app.exec_())
---
+++ my_dict.py
@@ -0,0 +1,79 @@
+travel_words = [
+    "Journey", "Voyage", "Expedition", "Tour", "Adventure", "Trip", "Excursion", "Exploration", 
+    "Trek", "Pilgrimage", "Jaunt", "Safari", "Sojourn", "Odyssey", "Ramble", "Roam", "Wander", 
+    "Hike", "Trekking", "Tourism", "Globetrotting", "Cruise", "Cruising", "Getaway", "Vacation", 
+    "Holiday", "Escape", "Retreat", "Excursion", "Outing", "Jaunt", "Explore", "Discover", "Visit", 
+    "Tourist", "Traveler", "Nomad", "Backpacker", "Wanderlust", "Itinerary", "Destination", 
+    "Passport", "Boarding pass", "Ticket", "Luggage", "Baggage", "Suitcase", "Backpack", 
+    "Guidebook", "Map", "Compass", "Camera", "Sightseeing", "Jet lag", "Layover", "Sunset", 
+    "Sunrise", "Airport", "Hotel", "Resort", "Hostel", "Motel", "Accommodation", "Camping", 
+    "Campsite", "Tent", "Campfire", "Route", "Highway", "Road trip", "Train", "Bus", 
+    "Airplane", "Cabin", "Countryside", "Cityscape", "Landscape", "Culture", "Tradition", 
+    "Heritage", "Local cuisine", "Street food", "Market", "Souvenir", "Beach", "Mountain", 
+    "Ocean", "Desert", "Forest", "Island", "Valley", "Hill", "Lake", "River", "Waterfall", 
+    "Canyon", "Cultural exchange", "Language barrier", "Native", "Foreign"
+]
+
+programming_words = [
+    "Code", "Programming", "Algorithm", "Software", "Development", "Debugging", "Syntax", 
+    "Function", "Variable", "Loop", "Conditional", "Object", "Class", "Method", "Interface", 
+    "Library", "Framework", "API", "Database", "Server", "Client", "Web", "Mobile", "Desktop", 
+    "Application", "Script", "Compiler", "Interpreter", "Version Control", "Git", "Repository", 
+    "Branch", "Merge", "Pull Request", "Bug", "Feature", "Issue", "Documentation", "Comment", 
+    "Testing", "Unit Test", "Integration Test", "Continuous Integration", "Deployment", 
+    "Agile", "Scrum", "Kanban", "Waterfall", "Sprint", "Iteration", "Refactoring", 
+    "Optimization", "Security", "Authentication", "Authorization", "Encryption", "Hashing", 
+    "Framework", "Framework", "Library", "Package", "Dependency", "Frontend", "Backend", 
+    "Full Stack", "Responsive Design", "User Interface", "User Experience", "Accessibility", 
+    "Responsive Design", "API Endpoint", "JSON", "XML", "HTTP", "HTTPS", "REST", "GraphQL", 
+    "Database", "SQL", "NoSQL", "MongoDB", "MySQL", "PostgreSQL", "Firebase", "Scalability", 
+    "Concurrency", "Parallelism", "Asynchronous", "Synchronous", "Thread", "Process", 
+    "Data Structure", "Algorithm", "Big O Notation", "Recursion", "Sorting", "Searching", 
+    "Linked List", "Tree", "Graph", "Stack", "Queue", "Hash Table", "Binary", "Bitwise","range", 
+    "Regular Expression", "Machine Learning", "Artificial Intelligence", "Neural Network"
+]
+
+family_words = [
+    "Family", "Parents", "Siblings", "Children", "Spouse", "Marriage", "Sister", "Brother", 
+    "Mother", "Father", "Son", "Daughter", "Grandparents", "Grandmother", "Grandfather", 
+    "Grandchildren", "Grandson", "Granddaughter", "Aunt", "Uncle", "Cousin", "Nephew", 
+    "Niece", "In-laws", "Mother-in-law", "Father-in-law", "Brother-in-law", "Sister-in-law", 
+    "Son-in-law", "Daughter-in-law", "Extended family", "Relatives", "Ancestors", "Descendants", 
+    "Family tree", "Home", "Household", "Love", "Bond", "Support", "Care", "Nurturing", 
+    "Unity", "Tradition", "Heritage", "Generations", "Celebration", "Gathering", "Mealtime", 
+    "Quality time", "Memories", "Connection", "Closeness", "Affection", "Hug", "Kiss", 
+    "Laughter", "Joy", "Happiness", "Conflict", "Resolution", "Understanding", "Respect", 
+    "Communication", "Sharing", "Chores", "Responsibility", "Discipline", "Guidance", 
+    "Education", "Upbringing", "Values", "Belonging", "Homecoming", "Family outing", 
+    "Vacation", "Holiday", "Family traditions", "Family bond", "Support system", "Home", 
+    "Safety", "Security", "Warmth", "Companionship", "Blood relations", "Blended family", 
+    "Adoption", "Stepfamily", "Single parent", "Parenting", "Childhood", "Adulthood", 
+    "Elderly", "Senior", "Caretaker", "Guardian", "Sacrifice", "Unconditional love"
+]
+
+art_words = [
+    "Art", "Creativity", "Expression", "Imagination", "Inspiration", "Aesthetics", 
+    "Artist", "Painter", "Sculptor", "Photographer", "Musician", "Dancer", "Actor", 
+    "Writer", "Poet", "Composer", "Visual Arts", "Fine Arts", "Performing Arts", 
+    "Contemporary Art", "Abstract Art", "Modern Art", "Classical Art", "Traditional Art", 
+    "Digital Art", "Mixed Media", "Sculpture", "Drawing", "Painting", "Portrait", 
+    "Landscape", "Abstract", "Canvas", "Palette", "Brush", "Easel", "Palette Knife", 
+    "Gallery", "Exhibition", "Museum", "Artwork", "Masterpiece", "Installation", 
+    "Curation", "Artistic Movement", "Impressionism", "Surrealism", "Cubism", 
+    "Realism", "Expressionism", "Pop Art", "Conceptual Art", "Performance Art", 
+    "Artistic Style", "Artistic Process", "Artistic Vision", "Composition", 
+    "Color Theory", "Texture", "Form", "Shape", "Line", "Space", "Balance", 
+    "Contrast", "Unity", "Harmony", "Medium", "Canvas", "Paper", "Wood", 
+    "Acrylic", "Oil", "Watercolor", "Charcoal", "Ink", "Digital Medium", 
+    "Photography", "Composition", "Shading", "Perspective", "Proportion", 
+    "Symmetry", "Asymmetry", "Rhythm", "Pattern", "Scale", "Lighting", 
+    "Shadow", "Mood", "Subject Matter", "Abstract Expressionism", 
+    "Artistic Movement", "Artistic Technique", "Artistic Influence", 
+    "Artistic Period", "Artistic Legacy", "Artistic Collaboration", 
+    "Artistic Community", "Art Critique", "Art Education", "Art Therapy", 
+    "Public Art", "Street Art", "Graffiti", "Interactive Art", 
+    "Art Collector", "Art Market", "Art Patron", "Art Auction", 
+    "Art Appreciation", "Art History", "Art Criticism"
+]
+
+theme_keywords={"travel":travel_words,"programming":programming_words,"family":family_words,"art":art_words}
